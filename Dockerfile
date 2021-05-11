56cabc2a2b5dec279a47fc353376be05363e606e
sridhar <sridhar@sridhar-tw.local>
2021-05-11 16:54:12 +0530

    Add express library

--- Dockerfile
+++ Dockerfile
@@ -8,5 +8,7 @@ RUN npm install
 
 COPY . .
 
+EXPOSE 8080
+
 CMD ["npm","start"]
 
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,16 @@
-console.log("Hello world from typescript")
+import express, { Request, Response } from "express";
+const bodyParser = require("body-parser");
+
+const app: express.Application = express();
+
+const PORT = 8080
+
+app.use(bodyParser.json());
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Welcome to Node.js server.");
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`);
+});
